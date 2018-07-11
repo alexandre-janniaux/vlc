a47ce0ced13aab95416e4f2b5161fbf9f94319d0
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2018-07-11 10:00:11 +0200

    demux: copy bitrate when packetizing

--- src/input/demux.c
+++ src/input/demux.c
@@ -522,6 +522,7 @@ decoder_t *demux_PacketizerNew( demux_t *p_demux, es_format_t *p_fmt, const char
 
     p_packetizer->fmt_in = *p_fmt;
     es_format_Init( &p_packetizer->fmt_out, p_fmt->i_cat, 0 );
+    p_packetizer->fmt_out.i_bitrate = p_fmt->i_bitrate;
 
     p_packetizer->p_module = module_need( p_packetizer, "packetizer", NULL, false );
     if( !p_packetizer->p_module )
