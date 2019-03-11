7f56b888c1748cc04b75cd1cd4dc8d37936804b5
Alexandre Janniaux <ajanni@videolabs.io>
2019-03-11 16:58:01 +0100

    WIP

--- src/misc/viewpoint.c
+++ src/misc/viewpoint.c
@@ -25,54 +25,71 @@
 #endif
 
 #include <vlc_viewpoint.h>
+#include <stdio.h>
 
 /* Quaternion to/from Euler conversion.
  * Original code from:
  * http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToEuler/ */
 static void QuaternionToEuler(float *yaw, float *pitch, float *roll, const float *q)
 {
-    float sqx = q[0] * q[0];
-    float sqy = q[1] * q[1];
-    float sqz = q[2] * q[2];
-    float sqw = q[3] * q[3];
+    float s1 = 2.f * (q[0] * q[1] + q[2] * q[3]);
+    float c1 = 1.f - 2.f * (q[1] * q[1] - q[2] * q[2]);
+    *roll = atan2(s1, c1);
 
-    // if the quaternion is normalised, unit is one
-    // otherwise it is correction factor
-    float unit = sqx + sqy + sqz + sqw;
-    float test = q[0] * q[1] + q[2] * q[3];
-
-    if (test > 0.499 * unit)
-    {
-        // singularity at north pole
-        *yaw = 2 * atan2(q[0], q[3]);
-        *roll = M_PI / 2;
-        *pitch = 0;
-    }
-    else if (test < -0.499 * unit)
-    {
-        // singularity at south pole
-        *yaw = -2 * atan2(q[0], q[3]);
-        *roll = -M_PI / 2;
-        *pitch = 0;
-    }
+    float s2 = 2.f * (q[0] * q[2] - q[3] * q[1]);
+    if (fabs(s2) >= 1)
+        *pitch = s2 > 0 ? M_PI_2 : -M_PI_2;
     else
-    {
-        *yaw   = atan2(2 * q[1] * q[3] - 2 * q[0] * q[2],
-                       sqx - sqy - sqz + sqw);
-        *roll  = asin(2 * test / unit);
-        *pitch = atan2(2 * q[0] * q[3] - 2 * q[1] * q[2],
-                       -sqx + sqy - sqz + sqw);
-    }
+        *pitch = asin(s2);
+
+    float s3 = 2.f * (q[0] * q[3] + q[1] * q[2]);
+    float c3 = 1.f - 2.f * (q[2] * q[2] + q[3] * q[3]);
+
+    *yaw = atan2(s3, c3);
+
+    //
+    //float sqx = q[0] * q[0];
+    //float sqy = q[1] * q[1];
+    //float sqz = q[2] * q[2];
+    //float sqw = q[3] * q[3];
+
+    //// if the quaternion is normalised, unit is one
+    //// otherwise it is correction factor
+    //float unit = sqx + sqy + sqz + sqw;
+    //float test = q[0] * q[1] + q[2] * q[3];
+
+    //if (test > 0.499 * unit)
+    //{
+    //    // singularity at north pole
+    //    *yaw = 2 * atan2(q[0], q[3]);
+    //    *roll = M_PI / 2;
+    //    *pitch = 0;
+    //}
+    //else if (test < -0.499 * unit)
+    //{
+    //    // singularity at south pole
+    //    *yaw = -2 * atan2(q[0], q[3]);
+    //    *roll = -M_PI / 2;
+    //    *pitch = 0;
+    //}
+    //else
+    //{
+    //    *yaw   = atan2(2 * q[1] * q[3] - 2 * q[0] * q[2],
+    //                   sqx - sqy - sqz + sqw);
+    //    *roll  = asin(2 * test / unit);
+    //    *pitch = atan2(2 * q[0] * q[3] - 2 * q[1] * q[2],
+    //                   -sqx + sqy - sqz + sqw);
+    //}
 }
 
 static void EulerToQuaternion(float *q, float yaw, float pitch, float roll)
 {
-    float c_yaw     = cos(yaw / 2.f);
-    float c_pitch   = cos(pitch / 2.f);
-    float c_roll    = cos(roll / 2.f);
-    float s_yaw     = sin(yaw / 2.f);
-    float s_pitch   = sin(pitch / 2.f);
-    float s_roll    = sin(roll / 2.f);
+    const float c_yaw   = cos(yaw / 2.f);
+    const float s_yaw   = sin(yaw / 2.f);
+    const float c_pitch = cos(pitch / 2.f);
+    const float s_pitch = sin(pitch / 2.f);
+    const float c_roll  = cos(roll / 2.f);
+    const float s_roll  = sin(roll / 2.f);
 
     q[0] = c_yaw * c_pitch * c_roll + s_yaw * s_pitch * s_roll;
     q[1] = c_yaw * c_pitch * s_roll - s_yaw * s_pitch * c_roll;
@@ -82,6 +99,42 @@ static void EulerToQuaternion(float *q, float yaw, float pitch, float roll)
 
 void vlc_viewpoint_to_4x4( const vlc_viewpoint_t *vp, float *m )
 {
+    float quat[] = { 0, 0, 0, 1.f };
+    float y, p, r;
+    QuaternionToEuler(&y, &p, &r, quat);
+    EulerToQuaternion(quat, y, p, r);
+
+    fprintf(stderr, "QUAT: %f %f %f %f | Euler %f %f %f \n", quat[0], quat[1], quat[2], quat[3], y, p, r);
+
+    memcpy(quat, &(float[4]){ 0, 0, 0, -1.f }, sizeof(quat));
+    QuaternionToEuler(&y, &p, &r, quat);
+    fprintf(stderr, "QUAT: %f %f %f %f | Euler %f %f %f \n", quat[0], quat[1], quat[2], quat[3], y, p, r);
+
+    y = 0; p = 0; r = 0;
+    EulerToQuaternion(quat, y, p, r);
+    fprintf(stderr, "QUAT: %f %f %f %f | Euler %f %f %f \n", quat[0], quat[1], quat[2], quat[3], y, p, r);
+
+    y = M_PI_2; p = 0; r = 0;
+    EulerToQuaternion(quat, y, p, r);
+    fprintf(stderr, "QUAT: %f %f %f %f | Euler %f %f %f \n", quat[0], quat[1], quat[2], quat[3], y, p, r);
+    QuaternionToEuler(&y, &p, &r, quat);
+    fprintf(stderr, "QUAT: %f %f %f %f | Euler %f %f %f \n", quat[0], quat[1], quat[2], quat[3], y, p, r);
+
+    y = 0; p = M_PI_2; r = 0;
+    EulerToQuaternion(quat, y, p, r);
+    fprintf(stderr, "QUAT: %f %f %f %f | Euler %f %f %f \n", quat[0], quat[1], quat[2], quat[3], y, p, r);
+    QuaternionToEuler(&y, &p, &r, quat);
+    fprintf(stderr, "QUAT: %f %f %f %f | Euler %f %f %f \n", quat[0], quat[1], quat[2], quat[3], y, p, r);
+
+    y = 0; p = 0; r = M_PI_2;
+    EulerToQuaternion(quat, y, p, r);
+    fprintf(stderr, "QUAT: %f %f %f %f | Euler %f %f %f \n", quat[0], quat[1], quat[2], quat[3], y, p, r);
+    QuaternionToEuler(&y, &p, &r, quat);
+    fprintf(stderr, "QUAT: %f %f %f %f | Euler %f %f %f \n", quat[0], quat[1], quat[2], quat[3], y, p, r);
+
+    //exit(1);
+
+
     /* The quaternion must be normalized */
     const float *q = vp->quat;
 
