fe09785d75cd6e13d59fa8e08b42b493a7d1866d
Adrien Maglo <magsoft@videolan.org>
2019-03-29 13:27:56 +0100

    clone: replace picture_Copy by picture_Hold

--- modules/video_splitter/clone.c
+++ modules/video_splitter/clone.c
@@ -184,14 +184,8 @@ static void Close( vlc_object_t *p_this )
 static int Filter( video_splitter_t *p_splitter,
                    picture_t *pp_dst[], picture_t *p_src )
 {
-    if( video_splitter_NewPicture( p_splitter, pp_dst ) )
-    {
-        picture_Release( p_src );
-        return VLC_EGENERIC;
-    }
-
     for( int i = 0; i < p_splitter->i_output; i++ )
-        picture_Copy( pp_dst[i], p_src );
+        pp_dst[i] = picture_Hold( p_src );
 
     picture_Release( p_src );
     return VLC_SUCCESS;
