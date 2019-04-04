1a3122b37418bf84347fa686aaf6cff6f4310897
Alexandre Janniaux <ajanni@videolabs.io>
2019-04-04 12:18:36 +0200

    HACK: egl: don't terminate context
    Replace this commit by the commit adding a proper usage of
    EGL_KHR_display_reference, which can't be upstream right now but is
    available for the GTX1070 with NVidia driver.

--- modules/video_output/opengl/egl.c
+++ modules/video_output/opengl/egl.c
@@ -185,7 +185,7 @@ static void Close(vlc_gl_t *gl)
             eglDestroyContext(sys->display, sys->context);
         if (sys->surface != EGL_NO_SURFACE)
             eglDestroySurface(sys->display, sys->surface);
-        eglTerminate(sys->display);
+        //eglTerminate(sys->display);
     }
 #ifdef USE_PLATFORM_X11
     if (sys->x11 != NULL)
