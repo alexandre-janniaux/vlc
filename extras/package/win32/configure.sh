82c5912113be6d55e3f865b8abd3a3d93f18abb1
Alexandre Janniaux <ajanni@videolabs.io>
2019-04-30 15:27:41 +0200

    WIP

--- extras/package/win32/configure.sh
+++ extras/package/win32/configure.sh
@@ -20,6 +20,7 @@ OPTIONS="
       --enable-sse --enable-mmx
       --enable-libcddb
       --enable-zvbi --disable-telx
+      --enable-vulkan
       --enable-nls"
 
 sh "$(dirname $0)"/../../../configure ${OPTIONS}  "$@"
