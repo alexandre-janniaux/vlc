011c44ce175dab4518efc4ce12adda2f0b3f5453
Alexandre Janniaux <ajanni@videolabs.io>
2019-05-17 19:13:59 +0200

    viewpoint: add test viewpoint

--- test/src/misc/viewpoint.c
+++ test/src/misc/viewpoint.c
@@ -19,7 +19,53 @@
  *****************************************************************************/
 
 #include <vlc_viewpoint.h>
+#include <math.h>
 #include "../../libvlc/test.h"
+
+
+static void mat4x4_for_angles( float *m, float *angles )
+{
+    float yaw   = angles[0] * (float)M_PI / 180.f + (float)M_PI_2;
+    float pitch = angles[1] * (float)M_PI / 180.f;
+    float roll  = angles[2] * (float)M_PI / 180.f;
+
+    float s, c;
+
+    s = sinf(pitch);
+    c = cosf(pitch);
+    const float x_rot[4][4] = {
+        { 1.f,    0.f,    0.f,    0.f },
+        { 0.f,    c,      -s,      0.f },
+        { 0.f,    s,      c,      0.f },
+        { 0.f,    0.f,    0.f,    1.f } };
+
+    s = sinf(yaw);
+    c = cosf(yaw);
+    const float y_rot[4][4] = {
+        { c,      0.f,    s,     0.f },
+        { 0.f,    1.f,    0.f,    0.f },
+        { -s,      0.f,    c,      0.f },
+        { 0.f,    0.f,    0.f,    1.f } };
+
+    s = sinf(roll);
+    c = cosf(roll);
+    const float z_rot[4][4] = {
+        { c,      s,      0.f,    0.f },
+        { -s,     c,      0.f,    0.f },
+        { 0.f,    0.f,    1.f,    0.f },
+        { 0.f,    0.f,    0.f,    1.f } };
+
+    /**
+     * Column-major matrix multiplication mathematically equal to
+     * z_rot * x_rot * y_rot
+     */
+    memset(m, 0, 16 * sizeof(float));
+    for (int i=0; i<4; ++i)
+        for (int j=0; j<4; ++j)
+            for (int k=0; k<4; ++k)
+                for (int l=0; l<4; ++l)
+                    m[4*i+l] += y_rot[i][j] * x_rot[j][k] * z_rot[k][l];
+}
 
 static bool
 reciprocal_euler(float epsilon, float yaw, float pitch, float roll)
@@ -35,13 +81,15 @@ reciprocal_euler(float epsilon, float yaw, float pitch, float roll)
     fprintf(stderr, "converted:  yaw=%f, pitch=%f, roll=%f\n", yaw2, pitch2, roll2);
     fprintf(stderr, "==========================================\n");
 
-    float d1 = fabs(yaw   - yaw2);
-    float d2 = fabs(pitch - pitch2);
-    float d3 = fabs(roll  - roll2);
+    float d1 = fmodf(360.f + (yaw   - yaw2),   360.f);
+    float d2 = fmodf(180.f + (pitch - pitch2), 180.f);
+    float d3 = fmodf(360.f + (roll  - roll2),  360.f);
 
-    return (d1 < epsilon || fabs(d1 - 90.f)  < epsilon) &&
-           (d2 < epsilon || fabs(d2 - 180.f)  < epsilon) &&
-           (d3 < epsilon || fabs(d3 - 180.f) < epsilon);
+    /* Check the two borders of the tore, 0.f and 180.f or 360.f
+     * depending on the range of the compared value. */
+    return (d1 < epsilon || 360.f - d1 < epsilon) &&
+           (d2 < epsilon || 180.f - d2 < epsilon) &&
+           (d3 < epsilon || 360.f - d3 < epsilon);
 }
 
 static void
@@ -49,10 +97,28 @@ test_conversion_euler_quaternion()
 {
     const float epsilon = 0.1f;
     assert(reciprocal_euler(epsilon, 0.f,  0.f,  0.f));
+    //assert(reciprocal_euler(epsilon, 90.f, 0.f,  0.f));
+    //assert(reciprocal_euler(epsilon, 0.f,  90.f, 0.f));
+    //assert(reciprocal_euler(epsilon, 0.f,  0.f,  90.f));
+    //assert(reciprocal_euler(epsilon, 90.f, 90.f, 00.f));
+    //assert(reciprocal_euler(epsilon, 90.f,  0.f, 90.f));
+    //assert(reciprocal_euler(epsilon, 0.f,  90.f, 90.f));
+    //assert(reciprocal_euler(epsilon, 90.f, 90.f, 90.f));
     assert(reciprocal_euler(epsilon, 45.f, 0.f,  0.f));
     assert(reciprocal_euler(epsilon, 0.f,  45.f, 0.f));
     assert(reciprocal_euler(epsilon, 0.f,  0.f,  45.f));
     assert(reciprocal_euler(epsilon, 45.f, 45.f, 0.f));
+
+    assert(reciprocal_euler(epsilon, 0.f,  0.f,  10.f));
+    assert(reciprocal_euler(epsilon, 0.f,  0.f,  20.f));
+    assert(reciprocal_euler(epsilon, 0.f,  0.f,  30.f));
+    assert(reciprocal_euler(epsilon, 0.f,  0.f,  40.f));
+    assert(reciprocal_euler(epsilon, 0.f,  0.f,  50.f));
+    assert(reciprocal_euler(epsilon, 0.f,  0.f,  60.f));
+    assert(reciprocal_euler(epsilon, 0.f,  0.f,  70.f));
+    assert(reciprocal_euler(epsilon, 0.f,  0.f,  80.f));
+    assert(reciprocal_euler(epsilon, 0.f,  0.f,  90.f));
+
     assert(reciprocal_euler(epsilon, 0.f,  45.f, 45.f));
     assert(reciprocal_euler(epsilon, 45.f, 45.f, 45.f));
 }
@@ -64,7 +130,7 @@ static int fuzzy_memcmp(const float *a, const float *b,
     {
         if (fabs(a[i]-b[i]) > epsilon)
         {
-            fprintf(stderr, "Difference at %d, a[%d]=%f, b[%d]=%f\n",
+            fprintf(stderr, "Difference at %zu, a[%zu]=%f, b[%zu]=%f\n",
                     i, i, a[i], i, b[i]);
             return a[i] < b[i] ? -1 : 1;
         }
@@ -79,6 +145,12 @@ struct example_mat4x4
 };
 
 struct example_mat4x4 examples_mat4x4[] = {
+    { .angles = {  0.f,   45.f,  45.f } },
+    { .angles = { 45.f,   45.f,  0.f  } },
+    { .angles = { 45.f,   0.f,   0.f  } },
+    { .angles = { -45.f,  0.f,   0.f  } },
+    { .angles = {  0.f,   45.f,  0.f  } },
+    { .angles = {  0.f,    0.f,  45.f } },
     { .angles = { 0.f, 0.f, 0.f },
       .mat    = {
           0.f,  0.f,  1.f,  0.f,
@@ -106,6 +178,8 @@ struct example_mat4x4 examples_mat4x4[] = {
          -1.f,  0.f,  0.f,  0.f,
           0.f, -1.f,  0.f,  0.f,
           0.f,  0.f,  0.f,  1.f, } },
+
+
 
     { .angles = { 90.f, 90.f, 0.f },
       .mat    = {
@@ -152,7 +226,8 @@ test_conversion_viewpoint_mat4x4()
            mat[8],  mat[9],  mat[10], mat[11], \
            mat[12], mat[13], mat[14], mat[15]);
 
-    for (int i=0; i<ARRAY_SIZE(examples_mat4x4); ++i)
+    bool success = true;
+    for (size_t i=0; i<ARRAY_SIZE(examples_mat4x4); ++i)
     {
         struct example_mat4x4 *ex = &examples_mat4x4[i];
         vlc_viewpoint_from_euler(&vp,
@@ -163,21 +238,36 @@ test_conversion_viewpoint_mat4x4()
         fprintf(stderr, "angles: %f %f %f\n",
                 ex->angles[0], ex->angles[1],
                 ex->angles[2]);
-        printmat("EXPECT", ex->mat);
+
+        float expect_mat[16];
+        mat4x4_for_angles(expect_mat, ex->angles);
+
+        //float transpose[16];
+        //for (int j=0; j<4; ++j)
+        //    for (int k=0; k<4; ++k)
+        //        transpose[4*k+j] = expect_mat[4*j+k];
+        //memcpy(expect_mat, transpose, sizeof(expect_mat));
+
+        bool diff = fuzzy_memcmp(mat, expect_mat,
+                                 ARRAY_SIZE(mat), epsilon);
+        //printmat("EXPECT", ex->mat);
+        if (!diff)
+            continue;
+
+        printmat("EXPECT COMPUTED", expect_mat);
         printmat("RESULT", mat);
-        assert(!fuzzy_memcmp(mat, ex->mat,
-                             ARRAY_SIZE(mat), epsilon));
-
-
+        success = false;
     }
+
+    assert(success);
 }
 
 int main( void )
 {
     test_init();
 
-    test_conversion_euler_quaternion();
     test_conversion_viewpoint_mat4x4();
+    test_conversion_euler_quaternion();
 
     return 0;
 }
