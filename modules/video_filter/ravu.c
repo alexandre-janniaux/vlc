8568cc581f920373f2eefc4297a16f3488dec839
Victorien Le Couviour--Tuffet <victorien@videolan.org>
2019-06-12 10:49:37 +0200

    Revert "vfilter: RAVU remove debug"
    This reverts commit 3f7da5416889026af30a340b3865b7566f4d06c3.

--- modules/video_filter/ravu.c
+++ modules/video_filter/ravu.c
@@ -1538,6 +1538,14 @@ Filter(filter_t *filter, picture_t *ipic)
                       sys->width, sys->height, sys->stride))
         goto error;
 
+#if 0
+    msg_Info(filter, "--------------------- START ------------------");
+    Filter_debug(sys->pass_0, sys->width, sys->height, sys->stride);
+    msg_Info(filter, "---------------------- END -------------------");
+#else
+    msg_Info(filter, "RAVU is alive and well my friend");
+#endif
+
     for (unsigned i = 0; i < sys->height; ++i)
         memcpy(opic->Y_PIXELS + i * opic->Y_PITCH,
                sys->pass_0 + i * sys->stride,
