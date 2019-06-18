f5a9a5881829e445d237970a6a3410d782b453c8
mmeisson <mmeisson@outlook.fr>
2019-06-18 11:09:08 +0200

    Starts to try to make some tls connection

--- modules/access/fbxapi.c
+++ modules/access/fbxapi.c
@@ -10,9 +10,22 @@
 #include <vlc_stream.h>
 #include <vlc_plugin.h>
 #include <vlc_input_item.h>
+#include <vlc_url.h>
+#include <vlc_tls.h>
 
-static int	connect( vlc_object_t * );
-static void	disconnect( vlc_object_t * );
+/*
+ * freebox's base uri is given by freebox itself
+ */
+#define BASE_URI_SIZE	16
+/*
+ * freebox's domain is given by freebox itself with format "garbage.fbxos.fr"
+ */
+#define DOMAIN_SIZE		32
+
+#define APP_NAME_SIZE	16
+
+static int	fbxapi_connect( vlc_object_t * );
+static void	fbxapi_disconnect( vlc_object_t * );
 
 vlc_module_begin()
 	set_shortname( "FBXAPI" )
@@ -21,7 +34,7 @@ vlc_module_begin()
 	set_category( CAT_INPUT )
     set_subcategory( SUBCAT_INPUT_ACCESS )
 	add_shortcut( "fbxapi", "fbx-api" )
-	set_callbacks( connect, disconnect )
+	set_callbacks( fbxapi_connect, fbxapi_disconnect )
 vlc_module_end()
 
 static ssize_t	fbxapi_read( stream_t *, void *, size_t );
@@ -31,72 +44,151 @@ static int		fbxapi_control( stream_t *, int, va_list );
 
 struct		s_fbxapi
 {
-	char	name[24];
+	struct
+	{
+		vlc_tls_t	*tls;
+	}				http;
+	/*
+	 * Informations about the api
+	 */
+	char			*api_base_uri;
+	char			*api_domain;
+	unsigned int	https_port;
+	int				api_version;
+
+	/*
+	 * Informations about the application
+	 */
+	char			*app_name;
+	char			*app_version;
+	char			*app_token;
+	char			*device_name;
+
+	/*
+	 *	Session woking on
+	 */
+	char			*session_id;
 };
 typedef struct s_fbxapi		s_fbxapi;
 
-
-static int
-connect( vlc_object_t *self )
+/*static int
+fbxapi_rest_connect( stream_t *p_access )
 {
-	stream_t		*p_access = (stream_t *)self;
-	s_fbxapi		*fbx = calloc(1, sizeof(*fbx));
+	const char	base_get_challenge[] = "/login";
+	const char	base_get_login[] = "/login/session";
+}*/
 
+static int		fbxapi_open_tls(stream_t *access)
+{
+	vlc_url_t		url;
+	s_fbxapi		*fbx;
+
+	fbx = access->p_sys;
+    if (
+		vlc_UrlParse(&url, access->psz_url)
+		|| url.psz_host == NULL
+		|| url.i_port == 0
+	)
+    {
+        msg_Err(access, "invalid location: %s", access->psz_location);
+        vlc_UrlClean(&url);
+        return VLC_EGENERIC;
+    }
+	fbx->http.tls = vlc_tls_SocketOpenTCP(self, url.psz_host, url.i_port);
+	vlc_UrlClean(&url);
+	if (fbx->http.tls == NULL)
+	{
+		free(fbx);
+		access->p_sys = fbx = NULL;
+		return VLC_ENOMEM;
+	}
+	return VLC_SUCCESS;
+}
+
+static int		fbxapi_connect( vlc_object_t *self )
+{
+	stream_t		*access = (stream_t *)self;
+	s_fbxapi		*fbx;
+	int				res;
+
+ 	access->p_sys = fbx = calloc(1, sizeof(*fbx));
 	if (fbx == NULL)
 	{
 		return VLC_ENOMEM;
 	}
-	p_access->p_sys = fbx;
-	printf("Function %s called\n", __FUNCTION__);
-	p_access->pf_readdir = fbxapi_readdir;
-	p_access->pf_read = fbxapi_read;
-	p_access->pf_control = access_vaDirectoryControlHelper;
-	p_access->pf_seek = fbxapi_seek;
+	res = fbx_open_tls(access);
+	if (res != VLC_SUCCESS)
+	{
+		return res;
+	}
+
+	access->pf_control = access_vaDirectoryControlHelper;
+	access->pf_seek = fbxapi_seek;
+
+/*	if (dummy++ == 0)
+	{
+		p_access->pf_readdir = fbxapi_readdir;
+	}
+	else
+	{
+		p_access->pf_read = fbxapi_read;
+	}
+*/
 	return VLC_SUCCESS;
 }
 
-static void
-disconnect( vlc_object_t *self )
+static void		fbxapi_disconnect( vlc_object_t *self )
 {
-	printf("Function %s called\n", __FUNCTION__);
+	stream_t		*p_access = (stream_t *)self;
+	s_fbxapi		*fbx = p_access->p_sys;
+
+	vlc_tls_SessionDelete(fbx->http.tls);
+	free(p_access->p_sys);
+	p_access->p_sys = fbx = NULL;
 	(void)self;
 }
 
-static int
-fbxapi_seek( stream_t *p_access, uint64_t u)
+static int		fbxapi_seek( stream_t *p_access, uint64_t u )
 {
 	(void)p_access;
 	return 1;
 }
 
-static int
-fbxapi_readdir( stream_t *p_access, input_item_node_t *current_node )
+static int		fbxapi_readdir(
+	stream_t *p_access,
+	input_item_node_t *current_node
+)
 {
-	const char	*uri = "fbxapi://where:4545?/tmp/path";
+	char	uri[] = "fbxapi://where:4545?/tmp/path9";
+	char	file_name[] = "/tmp/path4";
 
-	strcpy(((s_fbxapi *)p_access->p_sys)->name, "test");
-	printf("From %s -> '%s'\n", __FUNCTION__, ((s_fbxapi *)p_access->p_sys)->name);
+	printf(
+		"From %s -> '%s' %s\n",
+		__FUNCTION__,
+		p_access->psz_name,
+		p_access->psz_url
+	);
 
 	struct vlc_readdir_helper	rdh;
 
 	vlc_readdir_helper_init( &rdh, p_access, current_node );
 
-	vlc_readdir_helper_additem( &rdh, uri, "/tmp", "/tmp/path4", 1, ITEM_NET );
-	vlc_readdir_helper_additem( &rdh, uri, "/tmp", "/tmp/path5", 1, ITEM_NET );
-	vlc_readdir_helper_additem( &rdh, uri, "/tmp", "/tmp/path6", 1, ITEM_NET );
-	vlc_readdir_helper_additem( &rdh, uri, "/tmp", "/tmp/path7", 1, ITEM_NET );
-	vlc_readdir_helper_additem( &rdh, uri, "/tmp", "/tmp/path8", 1, ITEM_NET );
+	for (int i = 0; i < 5; i++)
+	{
+		uri[sizeof(uri) - 2] = (char)('0' + i);
+		file_name[sizeof(file_name) - 2] = (char)('0' + i);
+		vlc_readdir_helper_additem( &rdh, (uri), "/tmp", (file_name), 1, ITEM_NET );
+	}
 	vlc_readdir_helper_finish( &rdh, 1 );
 	return VLC_SUCCESS;
 }
 
-static ssize_t
-fbxapi_read( stream_t *p_access, void *buffer, size_t len )
+static ssize_t		fbxapi_read( stream_t *p_access, void *buffer, size_t len )
 {
 	const char sample[] = "gdsgjsfdlgfdgjsfdkghsfdjkghsfdjghsfdlkgjsfdlghsfdlghsfdg";
 	static int i = 0;
 
-	printf("From %s -> '%s'\n", __FUNCTION__, ((s_fbxapi *)p_access->p_sys)->name);
+	printf("From %s\n", __FUNCTION__);
 	if (i != 0)
 	{
 		return 0;
@@ -112,12 +204,6 @@ fbxapi_read( stream_t *p_access, void *buffer, size_t len )
 	);
 
 	input_item_t	*item = p_access->p_input_item;
-	printf(
-		"item '%s' '%s' %d \n",
-		item->psz_name,
-		item->psz_uri,
-		item->i_es
-	);
 
 	size_t		l = len >= sizeof(sample) ? sizeof(sample) : len;
 	memcpy(buffer, sample, l);
