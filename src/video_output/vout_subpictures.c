dc2afa3938354356ad1053cec111bd16d75ce320
Alexandre Janniaux <ajanni@videolabs.io>
2019-06-24 22:30:57 +0200

    vout_subpicture: copy raster size to output region

--- src/video_output/vout_subpictures.c
+++ src/video_output/vout_subpictures.c
@@ -994,6 +994,9 @@ static void SpuRenderRegion(spu_t *spu,
                                    (subpic->i_stop - fade_start);
         }
         dst->i_alpha   = fade_alpha * subpic->i_alpha * region->i_alpha / 65025;
+
+        dst->screen.i_width  = region->screen.i_width;
+        dst->screen.i_height = region->screen.i_height;
     }
 
 exit:
