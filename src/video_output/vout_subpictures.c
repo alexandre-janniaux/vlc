a4580f5664b9033be924398961d47ab0a394a169
Alexandre Janniaux <ajanni@videolabs.io>
2019-07-02 18:49:26 +0200

    vout_subpictures: copy output region sar

--- src/video_output/vout_subpictures.c
+++ src/video_output/vout_subpictures.c
@@ -1101,6 +1101,29 @@ static void SpuRenderRegion(spu_t *spu,
 
         dst->screen.i_width  = region->screen.i_width;
         dst->screen.i_height = region->screen.i_height;
+        if (external_scale)
+        {
+            dst->fmt.i_sar_num = region->fmt.i_sar_num;
+            dst->fmt.i_sar_den = region->fmt.i_sar_den;
+        }
+    }
+
+exit:
+    if (restore_text) {
+        /* Some forms of subtitles need to be re-rendered more than
+         * once, eg. karaoke. We therefore restore the region to its
+         * pre-rendered state, so the next time through everything is
+         * calculated again.
+         */
+        if (region->p_picture) {
+            picture_Release(region->p_picture);
+            region->p_picture = NULL;
+        }
+        if (region->p_private) {
+            subpicture_region_private_Delete(region->p_private);
+            region->p_private = NULL;
+        }
+        region->fmt = fmt_original;
     }
 }
 
