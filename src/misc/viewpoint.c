8eb462cd71b1bb512b3f2304e7780a68cbc6f5ec
Alexandre Janniaux <ajanni@videolabs.io>
2019-09-01 16:42:56 +0200

    WIP: quaternions

--- src/misc/viewpoint.c
+++ src/misc/viewpoint.c
@@ -27,25 +27,100 @@
 #include <vlc_viewpoint.h>
 #include <stdio.h>
 
-/* Quaternion to/from Euler conversion.
+/* Quaternion to Euler conversion.
  * Original code from:
  * http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToEuler/ */
 static void QuaternionToEuler(float *yaw, float *pitch, float *roll, const float *q)
 {
-    float s1 = 2.f * (q[0] * q[1] + q[2] * q[3]);
-    float c1 = 1.f - 2.f * (q[1] * q[1] - q[2] * q[2]);
-    *roll = atan2(s1, c1);
 
-    float s2 = 2.f * (q[0] * q[2] - q[3] * q[1]);
-    if (fabs(s2) >= 1)
-        *pitch = s2 > 0 ? M_PI_2 : -M_PI_2;
+    /* The matrix built from the angles is made from the multiplication of the
+     * following matrices:
+     *                    ⎡cos(yaw)  0  -sin(yaw)⎤
+     *  m_yaw (y_rot)   = ⎢   0      1      0    ⎥
+     *                    ⎣sin(yaw)  0  cos(yaw) ⎦
+
+     *                    ⎡1       0           0     ⎤
+     *  m_pitch (x_rot) = ⎢0  cos(pitch)   sin(pitch)⎥
+     *                    ⎣0  -sin(pitch)  cos(pitch)⎦
+
+     *                    ⎡cos(roll)   sin(roll)  0⎤
+     *  m_roll (z_rot)  = ⎢-sin(roll)  cos(roll)  0⎥
+     *                    ⎣    0           0      1⎦
+     *
+     * Which, multiplied in the correct order will bring, with the symbols
+     * rewritten: sin = s , cos = c, yaw = y, pitch = p, roll = r
+     *
+     *     ⎡s(p)⋅s(r)⋅s(y) + c(r)⋅c(y)  s(r)⋅c(p)  s(p)⋅s(r)⋅c(y) - s(y)⋅c(r)⎤
+     * V = ⎢s(p)⋅s(y)⋅c(r) - s(r)⋅c(y)  c(p)⋅c(r)  s(p)⋅c(r)⋅c(y) + s(r)⋅s(y)⎥
+     *     ⎣           s(y)⋅c(p)          -s(p)           c(p)⋅c(y)          ⎦
+     *
+     * We can first extract pitch = atan2( -V_32, sqrt(V_31^2 + V_33^2) )
+     *
+     * By taking the case |pitch| = 90 degree, it simplify c(y) and s(y) and:
+     *      roll = atan2( V_11, -V_21 )
+     *      yaw  = atan2( V_11, -V_13 )
+     *
+     * Otherwise, |pitch| != 90 degree and we can get:
+     *      roll = atan2( V_12, V_22 )
+     *      yaw  = atan2( V_31, V_33 )
+     *
+     * By identifying the coefficient in this matrix and the matrix obtained
+     * from converting the quaternion to 3x3 matrix, we get the following
+     * results.
+     */
+
+    /* Precompute square values as they are used multiple times. */
+    float sqx = q[0] * q[0];
+    float sqy = q[1] * q[1];
+    float sqz = q[2] * q[2];
+    float sqw = q[3] * q[3];
+
+    float gw = q[3];
+    float gx = q[0];
+    float gy = q[1];
+    float gz = q[2];
+
+    /* Diagonal values. */
+    float V_11 = 1 - 2 * (sqy + sqz);
+    float V_22 = 1 - 2 * (sqx + sqz);
+    float V_33 = 1 - 2 * (sqx + sqy);
+
+    /* Values with a minus sign. */
+    float V_31 = 2 * (gx * gz - gw * gy);
+    float V_12 = 2 * (gx * gy - gw * gz);
+
+    /* Values with only plus sign. */
+    float V_32 = 2 * (gw * gx + gy * gz);
+    float V_21 = 2 * (gx * gy + gw * gz);
+    float V_13 = 2 * (gw * gy + gx * gz);
+
+    *pitch = atan2( -V_32, sqrt( V_31*V_31 + V_33*V_33 ) );
+
+    if (fabs(*pitch) + 0.01f > (float)M_PI_2)
+    {
+        *roll = atan2( V_11, -V_21 );
+        *yaw  = atan2( V_11, -V_13 );
+    }
     else
-        *pitch = asin(s2);
+    {
+        *roll = atan2( V_12, V_22 );
+        *yaw  = atan2( V_31, V_33 );
+    }
 
-    float s3 = 2.f * (q[0] * q[3] + q[1] * q[2]);
-    float c3 = 1.f - 2.f * (q[2] * q[2] + q[3] * q[3]);
+    //float s1 = 2.f * (q[0] * q[1] + q[2] * q[3]);
+    //float c1 = 1.f - 2.f * (q[1] * q[1] - q[2] * q[2]);
+    //*roll = atan2(s1, c1);
 
-    *yaw = atan2(s3, c3);
+    //float s2 = 2.f * (q[0] * q[2] - q[3] * q[1]);
+    //if (fabs(s2) >= 1)
+    //    *pitch = s2 > 0 ? M_PI_2 : -M_PI_2;
+    //else
+    //    *pitch = asin(s2);
+
+    //float s3 = 2.f * (q[0] * q[3] + q[1] * q[2]);
+    //float c3 = 1.f - 2.f * (q[2] * q[2] + q[3] * q[3]);
+
+    //*yaw = atan2(s3, c3);
 
     //
     //float sqx = q[0] * q[0];
