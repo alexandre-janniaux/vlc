dd5fcc6335a649c39d193ea2854e8bf6651494db
David Loiret <david@videolabs.io>
2019-11-08 14:35:19 +0100

    http: improve playlist

--- share/lua/http/src/components/main-view/main-view.component.js
+++ share/lua/http/src/components/main-view/main-view.component.js
@@ -7,7 +7,21 @@ Vue.component('main-view', {
     },
     methods: {
         setLibraryView() {
-            this.$store.dispatch('layout/setLibraryView')
+            this.$store.dispatch('layout/setLibraryView');
         }
+    },
+    created() {
+        this.mainView === 'player' ? document.body.style['overflow-y'] = 'hidden' :
+            document.body.style['overflow-y'] = 'auto';
+        this.$store.subscribeAction((action, payload) => {
+            switch (action.type) {
+                case 'layout/setLibraryView':
+                    document.body.style['overflow-y'] = 'auto';
+                    break;
+                case 'layout/setPlayerView':
+                    document.body.style['overflow-y'] = 'hidden';
+                    break;
+            }
+        });
     }
 });
