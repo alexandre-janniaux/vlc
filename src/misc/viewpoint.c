9c596f7808eca4da6c23b042a03ddb9017d92e4a
Alexandre Janniaux <ajanni@videolabs.io>
2020-01-10 21:20:39 +0100

    viewpoint: cleanup

--- src/misc/viewpoint.c
+++ src/misc/viewpoint.c
@@ -116,41 +116,33 @@ static void QuaternionToEuler(float *yaw, float *pitch, float *roll, const float
     float V_33 = 1 - 2 * (sqx + sqy);
 
     /* Values with a minus sign. */
-    float V_31 = 2 * (gx * gz - gw * gy);
-    float V_12 = 2 * (gx * gy - gw * gz);
+    float V_13 = 2 * (gx * gz - gw * gy);
+    float V_21 = 2 * (gx * gy - gw * gz);
 
     /* Values with only plus sign. */
-    float V_32 = 2 * (gw * gx + gy * gz);
-    float V_21 = 2 * (gx * gy + gw * gz);
-    float V_13 = 2 * (gw * gy + gx * gz);
+    float V_23 = 2 * (gw * gx + gy * gz);
+    float V_12 = 2 * (gx * gy + gw * gz);
+    float V_31 = 2 * (gw * gy + gx * gz);
 
     if (test > 0.499 * unit)
     {
         // singularity at north pole
-        *yaw = -asin(M_11);
+        *yaw = -asin(V_11);
         *pitch = M_PI / 2;
         *roll = 0;
     }
     else if (test < -0.499 * unit)
     {
         // singularity at south pole
-        *yaw = asin(M_11);
+        *yaw = asin(V_11);
         *pitch = -M_PI / 2;
         *roll = 0;
     }
     else
     {
-        const float M_13 =  2.f * (q[0]*q[2] - q[3]*q[1]) / unit;
-        const float M_33 =  1.f - 2.f * (sqx + sqy) / unit;
-        *yaw   = atan2( M_13, M_33 );
-
-        const float M_23 = 2.f * (q[3]*q[0] + q[1]*q[2]) / unit;
-        const float M_21 = 2.f * (q[0]*q[1] - q[3]*q[2]) / unit;
-        const float M_22 = 1.f - 2.f * (sqx + sqz) / unit;
-        *pitch = atan2( -M_23, sqrt( M_21*M_21 + M_22*M_22 ) );
-
-        /* Roll = atan2( M_21, M_22 ) */
-        *roll  = -atan2( M_21, M_22 );
+        *yaw   = atan2( V_13, V_33 );
+        *pitch = atan2( -V_23, sqrt( V_21*V_21 + V_22*V_22 ) );
+        *roll  = -atan2( V_21, V_22 );
     }
 }
 
