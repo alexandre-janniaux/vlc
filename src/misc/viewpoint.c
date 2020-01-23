6c32525a7136b637b0993da52ae56b02f7029d17
Alexandre Janniaux <ajanni@videolabs.io>
2020-01-23 19:44:07 +0100

    WIP: use correct quaternion parameters

--- src/misc/viewpoint.c
+++ src/misc/viewpoint.c
@@ -110,39 +110,46 @@ static void QuaternionToEuler(float *yaw, float *pitch, float *roll, const float
     const float M_11 = 1.f - 2.f * (sqy + sqz) / unit;
 
     /* TODO not used currently */
+    /* CONVENTION:
+     * w, x, y, z lexical order on multiplication and addition */
+
     /* Diagonal values. */
     float V_11 = 1 - 2 * (sqy + sqz);
     float V_22 = 1 - 2 * (sqx + sqz);
     float V_33 = 1 - 2 * (sqx + sqy);
 
-    /* Values with a minus sign. */
-    float V_13 = 2 * (gx * gz - gw * gy);
-    float V_21 = 2 * (gx * gy - gw * gz);
-
-    /* Values with only plus sign. */
+    /* Values with a plus sign. */
+    float V_13 = 2 * (gw * gy + gx * gz);
+    float V_21 = 2 * (gw * gz + gx * gy);
     float V_23 = 2 * (gw * gx + gy * gz);
-    float V_12 = 2 * (gx * gy + gw * gz);
-    float V_31 = 2 * (gw * gy + gx * gz);
 
-    if (test > 0.499 * unit)
+    /* Values with a minus sign. */
+    float V_12 = 2 * (gw * gz - gx * gy);
+    float V_31 = 2 * (gw * gy - gx * gz);
+    float V_32 = 2 * (gw * gx - gy * gz);
+
+
+    if (V_32 > 0.990 * unit)
     {
         // singularity at north pole
-        *yaw = -asin(V_11);
-        *pitch = M_PI / 2;
+        // DOESNT WORK WITH ROLL=90
+        *yaw = -atan2( V_13, V_12 );
+        *pitch = M_PI_2;
         *roll = 0;
     }
-    else if (test < -0.499 * unit)
+    else if (V_32 < -0.999 * unit)
     {
         // singularity at south pole
-        *yaw = asin(V_11);
-        *pitch = -M_PI / 2;
+        *yaw = atan2( V_13, V_12);
+        *pitch = -M_PI_2;
         *roll = 0;
     }
     else
     {
-        *yaw   = atan2( V_13, V_33 );
-        *pitch = atan2( -V_23, sqrt( V_21*V_21 + V_22*V_22 ) );
-        *roll  = -atan2( V_21, V_22 );
+        *yaw   = atan2( V_31, V_33 );
+        *roll  = -atan2( V_12, V_22 );
+        *pitch = -asin( V_32 );
+        *pitch = atan2( -V_32, sqrt( V_33 * V_33 + V_31 * V_31 ) );
     }
 }
 
@@ -155,10 +162,10 @@ static void EulerToQuaternion(float *q, float yaw, float pitch, float roll)
     const float c_roll  = cos(roll / 2.f);
     const float s_roll  = sin(roll / 2.f);
 
-    q[3] =  c_yaw * c_pitch * c_roll - s_yaw * s_pitch * s_roll;
-    q[0] =  s_yaw * c_pitch * s_roll - c_yaw * s_pitch * c_roll;
-    q[1] = -s_yaw * c_pitch * c_roll - c_yaw * s_pitch * s_roll;
-    q[2] =  c_yaw * c_pitch * s_roll + s_yaw * s_pitch * c_roll;
+    q[3] =  c_yaw * c_pitch * c_roll + s_yaw * s_pitch * s_roll;
+    q[0] =  s_yaw * c_pitch * s_roll + c_yaw * s_pitch * c_roll;
+    q[1] =  s_yaw * c_pitch * c_roll - c_yaw * s_pitch * s_roll;
+    q[2] = -c_yaw * c_pitch * s_roll + s_yaw * s_pitch * c_roll;
 }
 
 void vlc_viewpoint_to_4x4( const vlc_viewpoint_t *vp, float *m )
@@ -181,7 +188,7 @@ void vlc_viewpoint_to_4x4( const vlc_viewpoint_t *vp, float *m )
 
     /* The quaternion is the opposite rotation of the view.
      * We need to inverse the matrix at the same time. */
-    m[0]  = xx + ww - yy - zz;
+    m[0]  = 1 - 2 * (yy + zz);
     m[1]  = 2 * (xy + zw);
     m[2]  = 2 * (xz - yw);
     m[3]  = 0;
