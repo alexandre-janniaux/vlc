2291ba71ce75c202b630d1e93d85bec199c80408
Alexandre Janniaux <ajanni@videolabs.io>
2020-02-17 14:53:12 +0100

    WIP: android: plugin: simplify code export

--- extras/package/android/plugin.c
+++ extras/package/android/plugin.c
@@ -1,18 +1,20 @@
 #include <stddef.h>
+#include <stdint.h>
+#define __LIBVLC__
+#define __PLUGIN__
+#define MODULE_STRING "vlcplugins"
+#include <vlc_plugin.h>
 typedef int (*vlc_set_cb) (void *, void *, int, ...);
 typedef int (*vlc_plugin_entry) (vlc_set_cb, void *);
 
 #define VLC_EXPORT __attribute__((visibility("default")))
 #define VLC_LOCAL __attribute__((visibility("hidden")))
 
-VLC_EXPORT
-const char vlc_module_name[] = "vlcplugins";
-
 VLC_LOCAL
 static const vlc_plugin_entry vlc_plugin_entries[];
 
-VLC_EXPORT
-int vlc_entry(vlc_set_cb func_set, void *opaque)
+EXTERN_SYMBOL DLL_SYMBOL \
+int CDECL_SYMBOL VLC_SYMBOL(vlc_entry)(vlc_set_cb func_set, void *opaque)
 {{
     for(const vlc_plugin_entry *entry=vlc_plugin_entries;
         *entry != NULL; entry++)
@@ -24,20 +26,5 @@ int vlc_entry(vlc_set_cb func_set, void *opaque)
     return 0;
 }}
 
-VLC_EXPORT
-const char * vlc_entry_license()
-{{
-    return "LICENSE";
-}}
-
-VLC_EXPORT
-const char * vlc_entry_copyright()
-{{
-    return "COPYRIGHT";
-}}
-
-VLC_EXPORT
-const char * vlc_entry_api_version()
-{{
-    return "4.0.4";
-}}
+VLC_METADATA_EXPORTS
+VLC_MODULE_NAME_HIDDEN_SYMBOL
