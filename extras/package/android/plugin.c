e8b0429eba4cd1d0cf7d0511ce9716ac7a0d7770
Alexandre Janniaux <ajanni@videolabs.io>
2020-02-21 19:25:09 +0100

    android: plugin: use #include for symbols

--- extras/package/android/plugin.c
+++ extras/package/android/plugin.c
@@ -10,8 +10,17 @@ typedef int (*vlc_plugin_entry) (vlc_set_cb, void *);
 #define VLC_EXPORT __attribute__((visibility("default")))
 #define VLC_LOCAL __attribute__((visibility("hidden")))
 
+#define VLC_PLUGIN(name) int name();
+#include "plugins.manifest.h"
+#undef VLC_PLUGIN
+
+#define VLC_PLUGIN(name) name,
 VLC_LOCAL
-static const vlc_plugin_entry vlc_plugin_entries[];
+static const vlc_plugin_entry vlc_plugin_entries[] = {
+#   include "plugins.manifest.h"
+    NULL
+};
+#undef VLC_PLUGIN
 
 EXTERN_SYMBOL DLL_SYMBOL \
 int CDECL_SYMBOL VLC_SYMBOL(vlc_entry)(vlc_set_cb func_set, void *opaque)
