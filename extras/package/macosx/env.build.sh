900f6525afab9509324d1fd37f9eeeb8aaeeced9
Alexandre Janniaux <ajanni@videolabs.io>
2020-05-03 20:16:55 +0200

    [2] WIP: cutme

--- extras/package/macosx/env.build.sh
+++ extras/package/macosx/env.build.sh
@@ -59,29 +59,29 @@ vlcSetSymbolEnvironment() {
     # List assumes macOS 10.10 / iOS 8 at minimum.
 
     # Added symbols in macOS 10.12 / iOS 10 / watchOS 3
-    export ac_cv_func_basename_r=no
-    export ac_cv_func_clock_getres=no
-    export ac_cv_func_clock_gettime=no
-    export ac_cv_func_clock_settime=no
-    export ac_cv_func_dirname_r=no
-    export ac_cv_func_getentropy=no
-    export ac_cv_func_mkostemp=no
-    export ac_cv_func_mkostemps=no
-    export ac_cv_func_timingsafe_bcmp=no
+    #export ac_cv_func_basename_r=no
+    #export ac_cv_func_clock_getres=no
+    #export ac_cv_func_clock_gettime=no
+    #export ac_cv_func_clock_settime=no
+    #export ac_cv_func_dirname_r=no
+    #export ac_cv_func_getentropy=no
+    #export ac_cv_func_mkostemp=no
+    #export ac_cv_func_mkostemps=no
+    #export ac_cv_func_timingsafe_bcmp=no
 
-    # Added symbols in macOS 10.13 / iOS 11 / watchOS 4 / tvOS 11
-    export ac_cv_func_open_wmemstream=no
-    export ac_cv_func_fmemopen=no
-    export ac_cv_func_open_memstream=no
-    export ac_cv_func_futimens=no
-    export ac_cv_func_utimensat=no
+    ## Added symbols in macOS 10.13 / iOS 11 / watchOS 4 / tvOS 11
+    #export ac_cv_func_open_wmemstream=no
+    #export ac_cv_func_fmemopen=no
+    #export ac_cv_func_open_memstream=no
+    #export ac_cv_func_futimens=no
+    #export ac_cv_func_utimensat=no
 
-    # Added symbol in macOS 10.14 / iOS 12 / tvOS 9
-    export ac_cv_func_thread_get_register_pointer_values=no
+    ## Added symbol in macOS 10.14 / iOS 12 / tvOS 9
+    #export ac_cv_func_thread_get_register_pointer_values=no
 
-    # Added symbols in macOS 10.15 / iOS 13 / tvOS 13
-    export ac_cv_func_aligned_alloc=no
-    export ac_cv_func_timespec_get=no
+    ## Added symbols in macOS 10.15 / iOS 13 / tvOS 13
+    #export ac_cv_func_aligned_alloc=no
+    #export ac_cv_func_timespec_get=no
 }
 
 vlcSetContribEnvironment() {
